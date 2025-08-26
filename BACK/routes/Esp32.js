fa999e3fd9517ff552f9cc993549a04b1e14f51a
joker2916 <yannmagadju30@gmail.com>
2025-08-26 05:58:37 +0100

    routes Esp32

--- BACK/routes/Esp32.js
+++ BACK/routes/Esp32.js
@@ -1,9 +1,13 @@
-// routes/Esp32.js
 const express = require("express");
 const router = express.Router();
 const controller = require("../controllers/Esp32");
 
-// Route pour que l'ESP32 envoie ses mesures
+// Route POST (l’ESP32 enverra ses mesures ici)
 router.post("/data", controller.receiveData);
+
+// Route GET (pour tester dans le navigateur)
+router.get("/data", (req, res) => {
+  res.json({ message: "✅ Endpoint ESP32 OK. Envoie tes données en POST." });
+});
 
 module.exports = router;
